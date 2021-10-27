29814bcedfc77c3d7898983333bcb800bdd6dcdd
BANGWON-LEE <qkddnjs31@gmail.com>
2021-10-27 21:50:10 +0900

    20211027 코드 정렬

--- src/components/menu/BoardCommentForm.jsx
+++ src/components/menu/BoardCommentForm.jsx
@@ -20,57 +20,54 @@ function BoardCommentForm({
             <StyledDiv3
                 style={{ backgroundColor: "gray", paddingTop: "10px", height: "100%" }}
             >
-
-
                 {boardComments && (
-                <div>
-                    {boardComments.map((boardComment) => (
-                    <BoardCommentsForm boardComment={boardComment} />
-                    ))}
-                </div>
+                    <div>
+                        {boardComments.map((boardComment) => (
+                        <BoardCommentsForm boardComment={boardComment} />
+                        ))}
+                    </div>
                 )}
-                                <StyledDiv2 style={{ paddingLeft: "7px" }}>
-                    
+                <StyledDiv2 style={{ paddingLeft: "7px" }}>
                     {commentDetail && (
-                        <StyledDiv2 style={{ paddingLeft: "12px", paddingTop: "5px" }}>
+                    <StyledDiv2 style={{ paddingLeft: "12px", paddingTop: "5px" }}>
                         {commentDetail.map((comment) => {
                             return (
-                            <div>
-                                <div style={{ float: "left", display: "inline-block" }}>
-                                {comment.name}
+                                <div>
+                                    <div style={{ float: "left", display: "inline-block" }}>
+                                    {comment.name}
+                                    </div>
+                                    <div
+                                        style={{ display: "inline-block", paddingRight: "45px" }}
+                                    >
+                                        {comment.content}
+                                    </div>
                                 </div>
-                                <div
-                                style={{ display: "inline-block", paddingRight: "45px" }}
-                                >
-                                {comment.content}
-                                </div>
-                            </div>
-                            );
+                                );
                         })}
-                        </StyledDiv2>
+                    </StyledDiv2>
                     )}
                 </StyledDiv2>
                 <StyledText3
-                        value={form.cm_content}
-                        onChange={onChange}
-                        onKeyPress={onKeyPress}
-                        name="cm_content"
-                        style={{
+                    value={form.cm_content}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                    name="cm_content"
+                    style={{
                         display: "inline-table",
                         marginRight: "0px",
                         paddingRight: "0px",
                         borderRightWidth: "0px",
-                        }}
-                    />
-                    <StyledButton1
-                        style={{ hegiht: "18px", fontSize: "11.5px", marginLeft: "0px" }}
-                        onClick={onSubmit}
-                    >
-                        등록
-                    </StyledButton1>
+                    }}
+                />
+                <StyledButton1
+                    style={{ hegiht: "18px", fontSize: "11.5px", marginLeft: "0px" }}
+                    onClick={onSubmit}
+                >
+                    등록
+                </StyledButton1>
             </StyledDiv3>
         </div>
-  );
+    );
 }
 
 export default BoardCommentForm;
