c9c55181aefddcbe99e852591c3a97f91d14ae3b
BANGWON-LEE <qkddnjs31@gmail.com>
2021-11-26 03:17:39 +0900

    20211126 서버 연결 작업

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 export const register = async ({mem_userid, mem_pass, mem_name, mem_hp}) =>
-    await axios.post('/3.36.66.15:5000/api/register', {mem_userid, mem_pass, mem_name, mem_hp});
+    await axios.post('http://3.36.66.15:5000/api/register', {mem_userid, mem_pass, mem_name, mem_hp});
 console.log("##");
 
 export const login = async ({mem_userid, mem_pass}) => 
-    await axios.post('/3.36.66.15:5000/api/login',{
+    await axios.post('http://3.36.66.15:5000/api/login',{
             mem_userid,
             mem_pass,
             
 });
 
 export const logout = async ({mem_userid}) => 
-    await axios.post('/3.36.66.15:5000/api/logout',{
+    await axios.post('http://3.36.66.15:5000/api/logout',{
     mem_userid,
 })
 
 
 export const userInfo = async ({mem_userid}) => 
-    await axios.get(`/3.36.66.15:5000/api/userInfo/${mem_userid}`)
+    await axios.get(`http://3.36.66.15:5000/api/userInfo/${mem_userid}`)
 
 
 
