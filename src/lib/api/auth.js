82544269d37185fec6410b11e296a308ae7b041d
BANGWON-LEE <qkddnjs31@gmail.com>
2021-11-26 03:09:38 +0900

    20211126 서버연결 작업

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 export const register = async ({mem_userid, mem_pass, mem_name, mem_hp}) =>
-    await axios.post('https://study-room-server.herokuapp.com/api/register', {mem_userid, mem_pass, mem_name, mem_hp});
+    await axios.post('3.36.66.15:5000/api/register', {mem_userid, mem_pass, mem_name, mem_hp});
 console.log("##");
 
 export const login = async ({mem_userid, mem_pass}) => 
-    await axios.post('https://study-room-server.herokuapp.com/api/login',{
+    await axios.post('3.36.66.15:5000/api/login',{
             mem_userid,
             mem_pass,
             
 });
 
 export const logout = async ({mem_userid}) => 
-    await axios.post('https://study-room-server.herokuapp.com/api/logout',{
+    await axios.post('3.36.66.15:5000/api/logout',{
     mem_userid,
 })
 
 
 export const userInfo = async ({mem_userid}) => 
-    await axios.get(`https://study-room-server.herokuapp.com/api/userInfo/${mem_userid}`)
+    await axios.get(`3.36.66.15:5000/api/userInfo/${mem_userid}`)
 
 
 
--- src/lib/api/menu.js
+++ src/lib/api/menu.js
@@ -1,42 +1,42 @@
 import axios from 'axios';
 
 export const zone = async ({st_idx,st_seatNumber,st_seatStatus,}) => 
-    await axios.get('https://study-room-server.herokuapp.com/api/zone',{
+    await axios.get('3.36.66.15:5000/api/zone',{
     st_idx,st_seatNumber,st_seatStatus, 
     });
 
 export const boardWrite = async ({ mem_idx, bd_title, bd_textarea}) => 
-    await axios.post('https://study-room-server.herokuapp.com/api/boardWrite', {
+    await axios.post('3.36.66.15:5000/api/boardWrite', {
     mem_idx, bd_title, bd_textarea 
 });
 
 export const boardList = async ({bd_idx, bd_title, mem_userid, bd_regDate }) => 
-    await axios.get('https://study-room-server.herokuapp.com/api/boardList', {
+    await axios.get('3.36.66.15:5000/api/boardList', {
     bd_idx, bd_title,  mem_userid, bd_regDate
 })
 
 export const boardContents = async ({bd_idx, bd_title, bd_cotents, bd_mem_idx, mem_userid, bd_regDate}) => 
-    await axios.get(`https://study-room-server.herokuapp.com/api/boardContents/${bd_idx}`, {
+    await axios.get(`3.36.66.15:5000/api/boardContents/${bd_idx}`, {
         bd_idx, bd_title, bd_cotents, bd_mem_idx, mem_userid, bd_regDate
     })
 
 export const boardComment = async ({cm_bd_idx, cm_content, cm_mem_idx })=> 
-    await axios.post('https://study-room-server.herokuapp.com/api/boardComment' , {
+    await axios.post('3.36.66.15:5000/api/boardComment' , {
         cm_bd_idx, cm_content, cm_mem_idx 
     })
 
 export const boardComments = async ({bd_idx}) => 
-    await axios.get(`https://study-room-server.herokuapp.com/api/boardComments/${bd_idx}` , {
+    await axios.get(`3.36.66.15:5000/api/boardComments/${bd_idx}` , {
         bd_idx
         
     })
     
 export const boardEdit = async ({  bd_title, bd_textarea, mem_idx, bd_idx}) => 
-    await axios.patch('https://study-room-server.herokuapp.com/api/boardEdit', {
+    await axios.patch('3.36.66.15:5000/api/boardEdit', {
     bd_title, bd_textarea, mem_idx, bd_idx
 })
 
 export const seat = async ({st_mem_idx, st_endDate, st_seatNumber, mem_userid}) => 
-    await axios.put('https://study-room-server.herokuapp.com/api/seat',{
+    await axios.put('3.36.66.15:5000/api/api/seat',{
     st_mem_idx, st_endDate, st_seatNumber, mem_userid
 })
