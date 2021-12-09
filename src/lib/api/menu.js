48ed9bc023a5480a71287bd11e8af95d07922e0e
BANGWON-LEE <qkddnjs31@gmail.com>
2021-12-09 23:04:33 +0900

    20211209 페이징 수정

--- src/lib/api/menu.js
+++ src/lib/api/menu.js
@@ -5,25 +5,25 @@ export const zone = async ({st_idx,st_seatNumber,st_seatStatus,}) =>
     st_idx,st_seatNumber,st_seatStatus, 
     });
 
-// export const boardWrite = async ({ mem_idx, bd_title, bd_textarea}) => 
-//     await axios.post('http://3.36.66.15:5000/api/boardWrite', {
-//     mem_idx, bd_title, bd_textarea 
-// });
-
 export const boardWrite = async ({ mem_idx, bd_title, bd_textarea}) => 
-    await axios.post('/api/boardWrite', {
+    await axios.post('http://3.36.66.15:5000/api/boardWrite', {
     mem_idx, bd_title, bd_textarea 
 });
 
-// export const boardList = async ({bd_idx, bd_title, mem_userid, bd_regDate }) => 
-//     await axios.get('http://3.36.66.15:5000/api/boardList', {
-//     bd_idx, bd_title,  mem_userid, bd_regDate
-// })
+// export const boardWrite = async ({ mem_idx, bd_title, bd_textarea}) => 
+//     await axios.post('/api/boardWrite', {
+//     mem_idx, bd_title, bd_textarea 
+// });
 
 export const boardList = async ({bd_idx, bd_title, mem_userid, bd_regDate, page }) => 
-    await axios.get(`/api/boardList/${page}`, {
+    await axios.get(`http://3.36.66.15:5000/api/boardList/${page}`, {
     bd_idx, bd_title,  mem_userid, bd_regDate, page
 })
+
+// export const boardList = async ({bd_idx, bd_title, mem_userid, bd_regDate, page }) => 
+//     await axios.get(`/api/boardList/${page}`, {
+//     bd_idx, bd_title,  mem_userid, bd_regDate, page
+// })
 
 export const boardContents = async ({bd_idx, bd_title, bd_cotents, bd_mem_idx, mem_userid, bd_regDate}) => 
     await axios.get(`http://3.36.66.15:5000/api/boardContents/${bd_idx}`, {
