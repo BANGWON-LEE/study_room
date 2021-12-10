d6f484ae1bc66fd1e9de020b900cf57d32fd623b
BANGWON-LEE <qkddnjs31@gmail.com>
2021-12-10 11:31:33 +0900

    20211210 게시판 리스트 수정

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ function App() {
         <Route exact component={TimeChoiceForm} path="/timeChoice" />
         <Route exact component={SeatPage} path="/seat" />
         <Route exact component={UserInfoPage} path="/userInfo/:mem_userid" />
-        <Route exact component={BoardListPage} path="/BoardList/:page"/>
+        <Route exact component={BoardListPage} path="/BoardList/:bd_page"/>
         <Route exact component={BoardWritePage} path="/BoardWrite"/>
         <Route exact component={BoardContentPage} path="/BoardContents/:bd_idx"/>
         <Route exact component={BoardEditPage} path="/BoardEdit/:bd_idx"/>
       </Switch>
-      </BrowserRouter> 
+    </BrowserRouter> 
   );
 }
 
